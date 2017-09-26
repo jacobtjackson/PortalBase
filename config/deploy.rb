e19138df8a9caea67797cdcdc17049457419a1e2
Jacob Jackson <jacob.t.jackson@gmail.com>
2017-09-26 17:51:33 -0500

    configure capfile and deploy.rb the right way

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.9.0"
 
 # Change these
-server '165.227.176.94', port: 8080, roles: [:web, :app, :db], primary: true
+server '165.227.176.94', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:jacobtjackson/PortalBase.git'
 set :application,     'PortalBase'
@@ -21,7 +21,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa_digitalocean) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
