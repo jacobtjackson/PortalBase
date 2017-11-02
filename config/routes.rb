84c7f11b7e891600b0abdc5569732eea7c3ced2d
Jacob Jackson <jacob.t.jackson@gmail.com>
2017-11-01 22:57:29 -0500

    one more route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   authenticate :user, lambda { |u| u.admin } do
-    mount Sidekiq::Web => ‘/sidekiq’
+    mount Sidekiq::Web => '/sidekiq'
   end
   devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
